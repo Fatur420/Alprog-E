dbe7a679e969181ad99e1f22ec22e52afcf641dc
Fatur420 <akufatur36@gmail.com>
2024-12-02 10:19:46 +0800

    Create Tugas6.py

---
+++ Tugas6.py
@@ -0,0 +1,23 @@
+# variable assigment value
+x = 9
+y = 6
+print("nilai dari x adalah : ", y)
+
+# aturan penamaan variable
+luas = 30 #bisa berupa kata
+patur_rahman = 100 #jika dua kata, digabung dengan "_"
+paturX = 10 #boleh gabungan capital non-kapital
+rahmanX = 3
+Patur22 = 10 #jika menggabungkan huruf dan angka, angka harus diakhir
+print("nilai dari Patur adalah : ", paturX)
+print("nilai dari patur adalah : ", rahmanX)
+print("nilai dari patur adalah : ", patur22)
+
+a = 4
+print("tipe data dari a : ", type(a)) #integer bilangan bulat
+b = 6.9
+print("tipe data dari b : ", type(b)) #float bilangan bulat
+c = "gacor"
+print("tipe data dari c : ", type(c)) #string, gabungan karakter
+d = False
+print("tipe data dari d : ", type(d)) #Boolean, true False
